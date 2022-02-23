659e75c36d7abe94745be2e5ad8b52e0cf1f4b27
att11 <att11@duke.edu>
2022-02-23 17:47:40 -0500

    added app to website

---
+++ serve_hw3.bat
@@ -0,0 +1,4 @@
+cd C:\Users\att11\PyCharmProjects\homework_3-1
+git pull https://%TESTAPP_GIT_PAT%@github.com/andrei-ty1/homework_3-1.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
