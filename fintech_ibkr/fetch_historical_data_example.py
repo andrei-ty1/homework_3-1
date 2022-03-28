ebdf086fe9e0e7e545fb744d8943357ba0835c6e
att11 <att11@duke.edu>
2022-03-28 18:06:59 -0400

    finished version of hw3 is on website

--- fintech_ibkr/fetch_historical_data_example.py
+++ fintech_ibkr/fetch_historical_data_example.py
@@ -6,14 +6,13 @@
 
 from ibapi.contract import Contract
 from fintech_ibkr import *
-import pandas as pd
 
-value = "EUR.USD"  # This is what your text input looks like on your app
+value = "EUR.USD" # This is what your text input looks like on your app
 
 # Create a contract object
 contract = Contract()
 contract.symbol = value.split(".")[0]
-contract.secType = 'CASH'
+contract.secType  = 'CASH'
 contract.exchange = 'IDEALPRO'  # 'IDEALPRO' is the currency exchange.
 contract.currency = value.split(".")[1]
 
@@ -24,4 +23,6 @@ historical_data = fetch_historical_data(contract)
 print(historical_data)
 
 # This script is an excellent place for scratch work as you figure this out.
-# historical_data.__str__().split(", ")
+
+
+
